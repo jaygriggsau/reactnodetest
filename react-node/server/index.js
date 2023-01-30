f9e1e32dd061eec78d27440f8c6a5d9186d513ec
jaygriggsau <110772005+jaygriggsau@users.noreply.github.com>
2023-01-30 08:01:56 +0000

    Reverting back some changes

--- react-node/server/index.js
+++ react-node/server/index.js
@@ -1,75 +1,45 @@
-const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
-const openai = require('openai');
+const express = require('express');
+const openai = require('openai')
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(bodyParser.json());
+  
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-openai.apiKey = "YOUR_OPENAI_API_KEY";
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
 app.post('/api/openai', async (req, res) => {
-  try {
-    const response = await openai.Completion.create({
-      prompt: req.body.text,
-      model: "text-davinci-002",
-      max_tokens:2048
-    });
-    res.json({ response: response.choices[0].text });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
+    try {
+        const configuration = new Configuration({
+          apiKey: process.env.REACT_APP_YOUR_OPENAI_API_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+        const response = await openai.createCompletion({
+          model: "text-davinci-003",
+          prompt: "Write a long-form piece in the style of belles-lettres about the subject, explore the different schools of thought on subject, share any tactics and strategies on the subject, and other interesting insights around the subject, who are the impactful people related to the subject, and what can we learn from them? The subject is." + subject + "Format in html with paragraph tags. Where possible add curiosity driven headings with H1 between sections.Style of belles-lettres. Format with spaces arund paragraphs and titles",
+          temperature: 0.7,
+          max_tokens: 2500,
+        });
+        setResult(response.data.choices[0].text);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Something went wrong' });
+    }
+  });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
-
-
-
-
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const express = require('express');
-// const openai = require('openai')
-
-// const PORT = process.env.PORT || 3001;
-
-// const app = express();
-// app.use(bodyParser.json());
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+})
   
-// // Have Node serve the files for our built React app
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-// // Handle GET requests to /api route
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hello from server!" });
-// });
-
-// app.post('/api/openai', async (req, res) => {
-//     try {
-//         const configuration = new Configuration({
-//           apiKey: process.env.REACT_APP_YOUR_OPENAI_API_KEY,
-//         });
-//         const openai = new OpenAIApi(configuration);
-//         const response = await openai.createCompletion({
-//           model: "text-davinci-003",
-//           prompt: "Write a long-form piece in the style of belles-lettres about the subject, explore the different schools of thought on subject, share any tactics and strategies on the subject, and other interesting insights around the subject, who are the impactful people related to the subject, and what can we learn from them? The subject is." + subject + "Format in html with paragraph tags. Where possible add curiosity driven headings with H1 between sections.Style of belles-lettres. Format with spaces arund paragraphs and titles",
-//           temperature: 0.7,
-//           max_tokens: 2500,
-//         });
-//         setResult(response.data.choices[0].text);
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ error: 'Something went wrong' });
-//     }
-//   });
-
-// // All other GET requests not handled before will return our React app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// })
-  
-//   app.listen(PORT, () => {
-//     console.log(`Server listening on ${PORT}`);
-//   });
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
