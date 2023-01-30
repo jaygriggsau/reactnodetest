63d604f7348d71100012db4d4283f7e2bfd9dac7
jaygriggsau <110772005+jaygriggsau@users.noreply.github.com>
2023-01-30 05:07:53 +0000

    Chasing Bugs

--- react-node/server/index.js
+++ react-node/server/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(bodyParser.json());
+
 openai.apiKey = process.env.REACT_APP_YOUR_OPENAI_API_KEY;
 
 // Have Node serve the files for our built React app
