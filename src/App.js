3dc59aa65766d373dc689dd10c66910178b29078
jaygriggsau <110772005+jaygriggsau@users.noreply.github.com>
2023-02-02 21:26:55 +0000

    Syntax Fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function Form() {
   const [text, setText] = useState('');
   const [response, setResponse] = useState('');
-  const [subject, setSubject] - useState('');
+  const [subject, setSubject] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
