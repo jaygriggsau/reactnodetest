aab8bb6f8eda3e1484d9a14bcde2d0ae878e7a5c
bharatsavani <savanibharat07@gmail.com>
2023-02-02 15:13:05 +0530

    Added new react setup

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function Form() {
+  const [text, setText] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('http://localhost:3001/api/openai', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text }),
+      });
+      const json = await res.json();
+      setResponse(json.response);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Submit Prompt</button>
+      {response && <section>{response}</section>}
+    </form>
+  );
+}
+
+export default Form;
