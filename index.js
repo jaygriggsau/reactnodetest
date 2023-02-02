3d781e78a0b601095b8844ef8b033ae9a55af82c
jaygriggsau <110772005+jaygriggsau@users.noreply.github.com>
2023-02-02 11:45:13 +0000

    Added input to prompt

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/api/openai', async (req, res) => {
       const openai = new OpenAIApi(configuration);
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Write a unit plan for the subject.  The plan should link to the Australian curriculum for their year level and subject. The subject is English, Year 10, reading and learning about \"to kill a mockingbird\".",
+        prompt: `Write a unit plan for the subject.  The plan should link to the Australian curriculum for their year level and subject. The subject is English, Year 10, reading and learning about "${req.body.text}".`,
         temperature: 0.7,
         max_tokens: 400,
         top_p: 1,
